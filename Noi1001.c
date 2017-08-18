4e4fef2063a2cb13cd67dcbfb6874e01de85eacd
Jinsong Bian <Jinsongbian@gmail.com>
2017-08-18 23:07:22 +0800

    Add Noi1000.c, encypt chars.

---
+++ Noi1001.c
@@ -0,0 +1,20 @@
+//
+//  Noi1001.c
+//  
+//
+//  Created by Jinsong Bian on 8/18/17.
+//
+//
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int a;
+    scanf("%c",&a);
+    a = 'z'+'a'-a;
+    printf("%c",a);
+    
+    return 0;
+}
